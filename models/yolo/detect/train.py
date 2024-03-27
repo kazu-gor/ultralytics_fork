9b6d93a1e27d4a7437b26b2fac0ca03631689d75
kazu-gor <ik126@icloud.com>
2024-03-27 13:06:28 +0900

    refactor(train.py): simplify code for creating a labeled training plot in DetectionTrainer class
    The code was simplified by removing duplicate lines and unnecessary parentheses. The `plot_labels` function is now called with the `boxes`, `cls`, `names`, `save_dir`, and `on_plot` arguments. This change improves code readability and reduces redundancy.

--- models/yolo/detect/train.py
+++ models/yolo/detect/train.py
@@ -147,13 +147,6 @@ class DetectionTrainer(BaseTrainer):
 
     def plot_training_labels(self):
         """Create a labeled training plot of the YOLO model."""
-        boxes = np.concatenate([lb["bboxes"]
-                               for lb in self.train_loader.dataset.labels], 0)
-        cls = np.concatenate([lb["cls"]
-                             for lb in self.train_loader.dataset.labels], 0)
-        plot_labels(boxes, cls.squeeze(
-        plot_labels(boxes, cls.squeeze(
-        ), names=self.data["names"], save_dir=self.save_dir, on_plot=self.on_plot)
-
-        plot_labels(boxes, cls.squeeze(
-        ), names=self.data["names"], save_dir=self.save_dir, on_plot=self.on_plot)
+        boxes = np.concatenate([lb["bboxes"] for lb in self.train_loader.dataset.labels], 0)
+        cls = np.concatenate([lb["cls"] for lb in self.train_loader.dataset.labels], 0)
+        plot_labels(boxes, cls.squeeze(), names=self.data["names"], save_dir=self.save_dir, on_plot=self.on_plot)
